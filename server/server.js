f644157af71e0ea036a70ce6b8f3e5031f7cbe72
Fazil-SA <fazilsa2000@gmail.com>
2022-12-05 13:28:50 +0530

    L3 MongoDB Atlas & Mongoose Connection

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
 
 const workoutRoutes = require('./routes/workouts')
 
@@ -16,7 +17,13 @@ app.use((req,res,next) => {
 //routes
 app.use('/api/workouts',workoutRoutes)
 
-//listen requests
-app.listen(process.env.PORT,() => {
-    console.log(`server started listening on port ${process.env.PORT}`)
-})
+//connect to db
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    //listen requests
+    app.listen(4000,() => {
+        console.log(`connected to db & listening on port ${process.env.PORT}`)
+        })
+    }).catch((err) => {
+        console.log(err)
+    })
+
