bac88a9ea7b3973ebf27f9953df333689b2163c0
Fazil-SA <fazilsa2000@gmail.com>
2022-12-05 06:18:34 +0530

    L2 - Express Route Setup

--- server/server.js
+++ server/server.js
@@ -1,19 +1,20 @@
 require('dotenv').config()
 const express = require('express')
 
+const workoutRoutes = require('./routes/workouts')
+
 //express app
 const app = express()
 
 //middlewares
+app.use(express.json())
 app.use((req,res,next) => {
     console.log(req.path,req.method)
     next()
 })
 
 //routes
-app.get('/',(req,res) => {
-    res.json({mssg : "hello"})
-})
+app.use('/api/workouts',workoutRoutes)
 
 //listen requests
 app.listen(process.env.PORT,() => {
